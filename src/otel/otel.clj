5807d16692be099d1ae6e8170b1c157fbd43ff3f
Ian McEwen <mian@cyverse.org>
2020-04-07 01:39:12 -0700

    Rearrange, add span fn

--- src/otel/otel.clj
+++ src/otel/otel.clj
@@ -8,14 +8,22 @@
   (let [tracer-provider (OpenTelemetry/getTracerProvider)]
     (.get tracer-provider "otel.otel clojure")))
 
-(defn span-scope
-  "Open a Scope for a Span, to be used with with-open"
-  [span]
-  (.withSpan (tracer) span))
-
 (def span-kinds
   {:internal Span$Kind/INTERNAL
    :server   Span$Kind/SERVER
    :client   Span$Kind/CLIENT
    :producer Span$Kind/PRODUCER
    :consumer Span$Kind/CONSUMER})
+
+(defn span
+  ([span-name]
+   (span span-name :internal))
+  ([span-name kind-key]
+   (-> (.spanBuilder (tracer) span-name)
+       (.setSpanKind (span-kinds kind-key))
+       (.startSpan))))
+
+(defn span-scope
+  "Open a Scope for a Span, to be used with with-open"
+  [span]
+  (.withSpan (tracer) span))
