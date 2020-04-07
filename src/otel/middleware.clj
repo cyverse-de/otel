d07c890f6a6fe5a9a9b577477cdf509c1f1e3573
Ian McEwen <mian@cyverse.org>
2020-04-07 16:34:36 -0700

    Add compojure middleware

---
+++ src/otel/middleware.clj
@@ -0,0 +1,16 @@
+(ns otel.middleware
+  (:require [otel.otel :as otel]
+            [clojure.string :as string]
+            [clojure.tools.logging :as log]))
+
+(defn otel-middleware
+  "Middleware that wraps an incoming request in a span describing the request"
+  [handler]
+  (fn [request]
+    (if-let [compojure-route (:compojure/route request)]
+      (let [span-name (string/join " " [(string/upper-case (name (compojure-route 0))) (compojure-route 1)])]
+        (otel/with-span [span [span-name :server]]
+          (handler request)))
+      (do
+        (log/warn "No compojure route information found, not adding opentelemetry span")
+        (handler request)))))
