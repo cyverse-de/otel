888591aff6a3f5fb21a54e512c089476011b4c8e
Ian McEwen <mian@cyverse.org>
2020-04-07 22:57:41 -0700

    Fix up middleware context handling for error handling

--- src/otel/http.clj
+++ src/otel/http.clj
@@ -1,4 +1,5 @@
 (ns otel.http
+  (:require [clojure.string :as string])
   (:import [io.opentelemetry.context.propagation HttpTextFormat$Getter HttpTextFormat$Setter]))
 
 (defn ring-getter
@@ -6,5 +7,5 @@
   (reify HttpTextFormat$Getter
     (get [this req k]
       (if (contains? (:headers req) k)
-        (first (get (:headers req) k))
+        (first (string/split (get (:headers req) k) #",")) ;; ring handles multiple headers with comma-joining, and at least traceparent can't have commas in it
         nil))))
--- src/otel/middleware.clj
+++ src/otel/middleware.clj
@@ -3,23 +3,41 @@
             [otel.http :as http]
             [clojure.string :as string]
             [clojure.tools.logging :as log])
-  (:import [io.grpc Context]
+  (:import [java.io Closeable]
+           [io.grpc Context]
            [io.opentelemetry OpenTelemetry]
            [io.opentelemetry.context ContextUtils]))
+
+(defn- do-span
+  [request handler]
+  (if-let [compojure-route (:compojure/route request)]
+    (let [span-name (string/join " "
+                                 [(string/upper-case (name (compojure-route 0)))
+                                  (compojure-route 1)])]
+          (otel/with-span [span [span-name :server]]
+            (handler request)))
+    (do
+      (log/warn "No compojure route information found, not adding opentelemetry span")
+      (handler request))))
+
+(defn- extract-context
+  [request]
+  (try
+    (ContextUtils/withScopedContext
+      (.extract
+        (.getHttpTextFormat (OpenTelemetry/getPropagators))
+        (Context/current)
+        request
+        (http/ring-getter)))
+    (catch Exception e
+      (log/warn "Got an exception trying to extract context from an incoming HTTP request", e)
+      (reify Closeable
+        (close [this] nil)))))
 
 (defn otel-middleware
   "Middleware that wraps an incoming compojure request in a span describing the
   request"
   [handler]
   (fn [request]
-    (if-let [compojure-route (:compojure/route request)]
-      (let [span-name (string/join " "
-                                   [(string/upper-case (name (compojure-route 0)))
-                                    (compojure-route 1)])
-            ctx (.extract (.getHttpTextFormat (OpenTelemetry/getPropagators)) (Context/current) request (http/ring-getter))
-            extracted-scope (ContextUtils/withScopedContext ctx)]
-        (otel/with-span [span [span-name :server]]
-          (handler request)))
-      (do
-        (log/warn "No compojure route information found, not adding opentelemetry span")
-        (handler request)))))
+    (with-open [_ (extract-context request)]
+      (do-span request handler))))
