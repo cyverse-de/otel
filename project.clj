1c9524f8f762069d0f93ab9e7552e12cbdc2793a
Ian McEwen <mian@cyverse.org>
2020-10-12 15:20:04 -0700

    Add Jenkins and docker/test files

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/cyverse-de/otel"
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :plugins [[test2junit "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [io.opentelemetry/opentelemetry-api "0.7.0"]])
---
+++ test/otel/otel_test.clj
@@ -0,0 +1,7 @@
+(ns otel.otel-test
+  (:require [clojure.test :refer :all]
+            [otel.otel :refer :all]))
+
+(deftest a-test
+  (testing "Dummy Test"
+    (is (= 1 1))))
