f86e7c861d3c3b082500531df4fc4ba351b1d85d
Ian McEwen <mian@cyverse.org>
2020-10-16 21:12:58 -0700

    Add deploy repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/cyverse-de/otel"
   :license {:name "BSD Standard License"
             :url "http://www.iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :plugins [[test2junit "1.2.2"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
