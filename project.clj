30fb787039f2637c5a6e77b97f141422b581e957
Ian McEwen <mian@cyverse.org>
2021-05-05 19:33:59 -0700

    Version 0.2.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/otel "0.2.4"
+(defproject org.cyverse/otel "0.2.5-SNAPSHOT"
   :description "A Clojure library wrapping the opentelemetry client for Java"
   :url "https://github.com/cyverse-de/otel"
   :license {:name "BSD Standard License"
