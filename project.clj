bcce13870e924c175aa63980a1fef389c02f2653
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-18 17:00:33 -0700

    CORE-2006: switched to the next snapshot version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/otel "0.2.6"
+(defproject org.cyverse/otel "0.2.7-SNAPSHOT"
   :description "A Clojure library wrapping the opentelemetry client for Java"
   :url "https://github.com/cyverse-de/otel"
   :license {:name "BSD Standard License"
